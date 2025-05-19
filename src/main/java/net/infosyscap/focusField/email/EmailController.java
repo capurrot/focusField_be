d533d22a75896111c5d6a95cdef5f85f303128dc
Timoteo Capurro <timoteo.capurro@infosyscap.net>
2025-05-19 10:12:57 +0200

    mail service

---
+++ src/main/java/net/infosyscap/focusField/email/EmailController.java
@@ -0,0 +1,22 @@
+package net.infosyscap.focusField.email;
+
+import jakarta.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/email")
+public class EmailController {
+    @Autowired
+    EmailService emailService;
+    @PostMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void sendEmail(@RequestBody EmailRequest request) throws MessagingException {
+        emailService.sendEmail(request.getTo(), request.getSubject(), request.getBody());
+    }
+}
+
