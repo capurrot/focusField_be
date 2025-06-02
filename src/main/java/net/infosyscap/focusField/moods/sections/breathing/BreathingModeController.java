a0044abd5ee1daa61f745009c9a95608045c0293
capurrot <timoteo.capurro@infosyscap.net>
2025-06-02 09:13:15 +0200

    aggiornato BreathingMode

--- src/main/java/net/infosyscap/focusField/moods/sections/breathing/BreathingModeController.java
+++ src/main/java/net/infosyscap/focusField/moods/sections/breathing/BreathingModeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/breathing-modes")
+@RequestMapping("/api/focus-field/breathing-modes")
 @RequiredArgsConstructor
 public class BreathingModeController {
 
